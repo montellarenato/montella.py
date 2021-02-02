c95f8951b4d0a5f259228557f8515e5f2fbc26e7
montellarenato <75393802+montellarenato@users.noreply.github.com>
2021-02-02 18:13:16 +0100

    Add files via upload

---
+++ lavoriPrimoQuadrimestre/pythonCicloWhlie1.2.py
@@ -0,0 +1,17 @@
+x = int(input("inserisci un primo numero"))
+
+y= int(input("inserisci un primo numero"))
+
+n = x + 1
+
+while x < n < y:
+
+    print(n)
+
+    n = n + 1
+
+if x == ValueError:
+     print("inserire solo numeri interi")
+
+if y == ValueError:
+     print("inserire solo numeri interi")
