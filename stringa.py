01f3a8b8cfac3e38ba738f615705463f83a6baf1
montellarenato <75393802+montellarenato@users.noreply.github.com>
2021-02-02 18:13:57 +0100

    Delete stringa.py

--- stringa.py
+++
@@ -1,9 +0,0 @@
-frase = str(input("inserisci una stringa"))
-
-alfabeto = ["a","b","c","d"]
-
-
-for carattere in frase:
-    if carattere in alfabeto:
-        print("la lettera che compaie più volte è " + l )
-
