2ecf99c6f6b7a518e88680f9b8217d0cfba0fe7c
montellarenato <75393802+montellarenato@users.noreply.github.com>
2021-02-03 20:27:56 +0100

    Add files via upload

---
+++ lavoriPrimoQuadrimestre/stringa.py
@@ -0,0 +1,9 @@
+frase = str(input("inserisci una stringa"))
+
+alfabeto = ["a","b","c","d"]
+
+
+for carattere in frase:
+    if carattere in alfabeto:
+        print("la lettera che compaie più volte è " + l )
+
