30695f322ed33a9efed01b9a2cc4da1189f674c5
montellarenato <75393802+montellarenato@users.noreply.github.com>
2021-03-10 16:46:31 +0100

    Add files via upload

---
+++ esercizi2/esercitazione2.1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+d1=(input("inserisci un numero intero"))
+d2=(input("inserisci un'altro numeri intero"))
+d3=(input("mi aggiunga un numero intero"))
+d4=(input("inserisci ancora un'altro numero intero"))
+d5=(input("inserisci l'ultimo numero intero"))
+
+array=[d1, d2, d3, d4, d5]
+
+array.sort()
+
+array.reverse()
+
+print(array)
